fa44b614cbc78b638172d3ca9df5b1c4a349f900
SullaFelix <subasifurkan@gmail.com>
2019-11-28 00:13:36 +0300

    swagger and gitignore changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableAsync
+@EnableSwagger2
 public class DemoApplication {
 
     @Bean
--- src/main/java/com/example/demo/web/ExchangeRateController.java
+++ src/main/java/com/example/demo/web/ExchangeRateController.java
@@ -5,6 +5,7 @@ import com.example.demo.model.ExchangeRate;
 import com.example.demo.model.Transaction;
 import com.example.demo.service.ExchangeRateLookupService;
 import com.example.demo.service.TransactionService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -34,12 +35,14 @@ public class ExchangeRateController {
     }
 
     @GetMapping("rate")
+    @ApiOperation("This service call is used to get the exchange rate, currency pair input is given as query parameters, 'base' and 'target'")
     public ExchangeRate rate(@RequestParam(value = "base", required = true) String baseCode,
                              @RequestParam(value = "target", required = true) String targetCode) {
         return exchangeRateLookupService.getExchangeRate(baseCode, targetCode);
     }
 
     @GetMapping("convert")
+    @ApiOperation("This service call is used to convert an amount in source currency to the amount in target currency")
     public Transaction convert(@RequestParam(value = "base", required = true) String baseCode,
                                @RequestParam(value = "target", required = true) String targetCode,
                                @RequestParam(value = "amount", required = true) BigDecimal amount) {
@@ -52,6 +55,9 @@ public class ExchangeRateController {
     }
 
     @GetMapping("conversions")
+    @ApiOperation("This service call is used to get the list of conversions performed. Either a transaction id or " +
+            "transaction date must be given as query parameter parameters 'transactionId' and 'date' respectively. " +
+            "Paging is controlled by the page query parameter, starting with 0 i.e. '?page=0'")
     private Page<Transaction> transactions(
             @RequestParam(value = "transactionId", required = false) UUID transactionId,
             @RequestParam(value = "date", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
